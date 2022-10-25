b4df15f46afaa627c283ee36af7ecd59fa9e1792
sayed_safi2000 <safiuddin102030@gmail.com>
2022-10-25 20:42:00 +0600

    added-category-by-using-id

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-
+const course = require("./Data/course.json");
+const cors = require("cors");
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get("/course", (req, res) => {
+    res.send(course);
+});
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c.id === id);
+    console.log(selectedCourse)
+    res.send(selectedCourse);
+})
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.filter(c => c.category_id === id);
+    console.log(selectedCourse)
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
